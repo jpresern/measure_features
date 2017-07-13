6dac50bb70fe6928d0349878b8b8f62e820596b0
jpresern <jpresern@users.noreply.github.com>
2017-07-13 15:01:35 +0200

    measuring of CI added

---
+++ ci.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import tkinter as tk
+import matplotlib
+
+matplotlib.use("TkAgg")
+
+import sys, os, glob
+import re
+import pandas as pd
+import numpy as np
+import math
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatch
+import matplotlib.image as mimg
+import matplotlib.path as mpath
+from itertools import product, compress
+from datetime import datetime
+from optparse import OptionParser
+from tkinter.filedialog import askopenfilename, asksaveasfilename, askdirectory
+from datetime import datetime
+
+from IPython import embed
+
+from measure_features import get_things_opened, bulk_measure_ci, image_display, prepare_storage,\
+    prepare_short_storage
+
+
+__author__ = "bpiskur, jpresern"
+
+
+def get_folder_opened():
+    idir = './samples'
+    window_open = tk.Tk()
+    window_open.withdraw()
+    im_name = askdirectory(initialdir=idir, title='Select image file')
+    window_open.destroy()
+
+    return im_name
+
+
+def get_bulk_saved(store, store_short, dir):
+    """ get filename """
+    idir = dir.split('/')[-1]
+    window_save = tk.Tk()
+    window_save.withdraw()
+    save_fn = dir + '/' + idir
+
+    window_save.destroy()
+
+    store.to_csv(save_fn + '.csv', index_label="index")
+    store_short.to_csv(save_fn + '_short.csv', index_label="index")
+    """ resets instructions to nothing before save"""
+
+
+if __name__ == '__main__':
+
+    """ declare myself """
+
+    print("=============================================================")
+    print("|    Measure features - Simple tool for simple jobs v.1.1   |")
+    print("|                                                           |")
+    print("|                          modul CI                         |")
+    print("|                                                           |")
+    print("|    by: Barbara Piskur and Janez Presern (c) 2016, 2017    |")
+    print("|                                                           |")
+    print("|    read README.md for help and instructions               |")
+    print("=============================================================")
+    args = sys.argv
+    to_be_parsed = args[1:]
+
+    """ define options parser """
+    parser = OptionParser()
+    parser.add_option("-f", "--file", action="store", type="string", dest="filename",
+                      default='')
+
+    (options, args) = parser.parse_args(args)
+
+    """ outer main loop """
+    do_work = True
+    while do_work:
+
+        directory = get_folder_opened()
+
+        wings = []
+
+        if not wings:
+            wings = glob.glob(directory + '/*.bmp')
+        elif not wings:
+            wings = glob.glob(directory + '/*.JPG')
+        elif not wings:
+            wings = glob.glob(directory + '/*.tif')
+        else:
+            print('No files found')
+
+        """ prepare empty storage """
+        storage = prepare_storage()
+        storage_short = prepare_short_storage()
+        counter = 0
+
+        for wing in wings:
+            """ sort extensions etc """
+            fn = wing.rsplit('.', maxsplit=1)[0]
+            ext = wing.rsplit('.', maxsplit=1)[1]
+            ext = '.' + ext
+            title_fn = wing.rsplit('/', maxsplit=1)[-1]
+
+            """ load image, load meta data, display image and calibrate """
+            fig, ax2, im_index, pix_size = image_display(wing, fn)
+
+            """ action """
+            """ main loop """
+
+            fig.canvas.manager.window.deiconify()
+            fig.canvas.manager.window.tkraise()
+            storage, storage_short, counter = bulk_measure_ci(title_fn, fig, ax2, storage, storage_short, pix_size,
+                                                         counter)
+            plt.close(fig)
+            counter += 1
+        get_bulk_saved(storage, storage_short, directory)
+        und_jetzt = input('And now what? (L)oad new samples, or (Q)uit?\n')
+        if (und_jetzt == 'q') or (und_jetzt == 'Q'):
+            do_work = False
+
+    exit()
--- measure_features.py
+++ measure_features.py
@@ -19,10 +19,72 @@ from itertools import product, compress
 from datetime import datetime
 from optparse import OptionParser
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+from datetime import datetime
 
 from IPython import embed
 
 __author__ = "bpiskur, jpresern"
+
+
+def order_points(pts):
+    # list of coordinates 1: top-left,
+    # 2: top-right, 3:bottom-right, 4: bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
 
 
 def image_display(image_fn, meta_fn):
@@ -179,6 +241,23 @@ def store_distance(df, df2, file, xy_pairs, distance, parallel=1):
     return pd.concat([df, df_empty], ignore_index=True), pd.concat([df2, df_empty2], ignore_index=True)
 
 
+def store_ci(df, df2, file, xy_pairs, ci, parallel=1):
+
+    columns = ['datetime', 'sample', 'parallel', 'type', 'element', 'x', 'y', 'quality', 'quantity']
+    columns2 = ['datetime', 'sample', 'parallel', 'quality', 'quantity']
+    df_empty = pd.DataFrame(columns=columns)
+    df_empty2 = pd.DataFrame(columns=columns2)
+    df_empty.loc[0, :] = datetime.now(), file, parallel, 'ci', 'start_point', xy_pairs[0, 0], xy_pairs[0, 1], \
+                         'ci', ci
+    df_empty.loc[1, :] = datetime.now(), file, parallel, 'ci', 'common_point', xy_pairs[1, 0], xy_pairs[1, 1], \
+                         'ci', ci
+    df_empty.loc[2, :] = datetime.now(), file, parallel, 'ci', 'end_point', xy_pairs[2, 0], xy_pairs[2, 1], \
+                         'ci', ci
+    df_empty2.loc[0, :] = datetime.now(), file, parallel, 'ci', ci
+
+    return pd.concat([df, df_empty], ignore_index=True), pd.concat([df2, df_empty2], ignore_index=True)
+
+
 def drawing_board():
     """ create image with axes """
 
@@ -194,6 +273,8 @@ def drawing_board():
 
 
 def calibrate(figa, ax):
+    figa.canvas.manager.window.deiconify()
+    figa.canvas.manager.window.tkraise()
     ax.set_title('Click at the beginning and at the end of scale bar')
     x = figa.ginput(n=2)
     x1 = x[0][0]
@@ -291,14 +372,26 @@ def read_in_settings(file_name, fig, ax2):
                         print('Length of scale bar in micrometers:\n', barsize)
 
     except:
-        pass
-
-    if (barsize == None) | (barsize_pixels == None):
-        print('There are no calibration data in log file. Starting calibration')
-        ax2.set_title('There are no calibration data in log file. Starting calibration')
-        barsize, barsize_pixels, pix_size = calibrate(fig, ax2)
+        print('There are no calibration data. Everything will be set at value 1')
         magnification = 1
+        barsize_pixels = 1
+        barsize = 1
+        zeit = None
+        date = None
 
+
+
+    # if (barsize == None) | (barsize_pixels == None):
+    #     print('There are no calibration data in log file.')
+    #     want_calib = input('Calibrate? Y/N\n')
+    #     if want_calib == 'y':
+    #         get_things_saved(fig, storage, storage_short, filename, title_fn)
+    #         ax2.set_title('There are no calibration data in log file. Starting calibration')
+    #         barsize, barsize_pixels, pix_size = calibrate(fig, ax2)
+    #     elif want_calib == 'n':
+    #         barsize, barsize_pixels, pix_size, magnification = 1, 1, 1, 1
+    #         zeit = None
+    #         date = None
     return magnification, barsize, barsize_pixels, zeit, date
 
 
@@ -425,6 +518,77 @@ def measure_distance(file, figa, axis2, store, store_short, pix=1, count=0):
     return store, store_short, count
 
 
+def measure_ci(file, figa, axis2, store, store_short, pix=1, count=0):
+    axis2.set_title('Select three points of CI')
+
+    figa.canvas.manager.window.deiconify()
+    figa.canvas.manager.window.tkraise()
+    xy = np.asarray(figa.ginput(n=3, timeout=0))
+    dist0 = np.linalg.norm(xy[0] - xy[1])
+    dist0 *= pix
+    dist1 = np.linalg.norm(xy[1] - xy[2])
+    dist1 *= pix
+    ci = dist0/dist1
+    print('CI: ' + str(np.round(ci,2)) +'\n')
+    linija0 = axis2.plot(xy[:, 0], xy[:, 1], marker="+", markersize=8, color='g')
+    # linija1 = axis2.plot(xy[:,1], xy[:, 2], marker="+", markersize=8, color='r')
+
+    axis2.text(np.mean(xy, axis=0)[0], np.mean(xy, axis=0)[1], str(np.round(ci, 2)),
+               color=linija0[0].get_color())
+    figa.canvas.manager.window.iconify()
+    axis2.set_title('Are you happy? Press Y to store data or N to drop them')
+    happiness = input('Are you happy? Press Y to store data or N to drop them\n')
+
+    if happiness == 'y':
+        axis2.set_title('STORING data')
+        store, store_short = store_ci(store, store_short, file, xy, ci=ci, parallel=count)
+        count += 1
+
+    elif happiness == 'n':
+        del ci, dist0, dist1
+        linija0[0].remove()
+
+    axis2.set_title('')
+    return store, store_short, count
+
+
+def bulk_measure_ci(file, figa, axis2, store, store_short, pix=1, count=0):
+    happiness = 'n'
+    while happiness == 'n':
+        axis2.set_title(file + ': Select three points of CI')
+        figa.canvas.manager.window.deiconify()
+        figa.canvas.manager.window.tkraise()
+        xy = np.asarray(figa.ginput(n=3, timeout=0))
+        dist0 = np.linalg.norm(xy[0] - xy[1])
+        dist0 *= pix
+        dist1 = np.linalg.norm(xy[1] - xy[2])
+        dist1 *= pix
+        ci = dist0/dist1
+        print('CI: ' + str(np.round(ci,2)) +'\n')
+        linija0 = axis2.plot(xy[:, 0], xy[:, 1], marker="+", markersize=8, color='g')
+        # linija1 = axis2.plot(xy[:,1], xy[:, 2], marker="+", markersize=8, color='r')
+
+        tekst0 = axis2.text(np.mean(xy, axis=0)[0], np.mean(xy, axis=0)[1], str(np.round(ci, 2)),
+                   color=linija0[0].get_color())
+        figa.canvas.manager.window.iconify()
+        axis2.set_title('Are you happy? Press Y to store data or N to drop them')
+        happiness = input('Are you happy? Press Y to store data or N to drop them\n')
+
+        if happiness == 'n':
+            del ci, dist0, dist1
+            linija0[0].remove()
+            tekst0[0].remove()
+
+        if happiness == 'y':
+            axis2.set_title('STORING data')
+            store, store_short = store_ci(store, store_short, file, xy, ci=ci, parallel=count)
+            count += 1
+
+    axis2.set_title('')
+    return store, store_short, count
+
+
+
 def redraw_stored_things(axis2, store):
 
     """ draw selecteda areas with marked features
@@ -448,8 +612,7 @@ def redraw_stored_things(axis2, store):
         element_type = iter_element["type"].unique().tolist()
 
         if element_type.__contains__("area"):
-            axis2.add_patch(mpatch.Polygon(iter_element.loc[(iter_element["type"] == "area"), ["x", "y"]].values,
-                                           facecolor=farba[count], alpha=0.2))
+
             if element_type.__contains__("points"):
                 points = iter_element.loc[(iter_element["type"] == "points"), ["x", "y"]].values
                 axis2.plot(points[:, 0], points[:, 1], linestyle="", marker="+", markersize=8, color=farba[count])
@@ -485,12 +648,31 @@ def load_measurements(file, file2):
     return store, store_short, figa, axis, pix, im_ix
 
 
+def stretch_comb(file, figa, axa2):
+    """
+    Stretch out comb photo
+    :param file:
+    :param figa:
+    :param axa2:
+    :return:
+    """
+
+
+    figa.canvas.manager.window.deiconify()
+    figa.canvas.manager.window.tkraise()
+    figa.suptitle(file)
+    axa2.set_title('Select corners of the comb you will be counting bees on. Press ENTER when done')
+    x = np.asarray(fig.ginput(n=4, timeout=0))
+
+    comb = four_point_transform(file, x)
+    return comb
+
 if __name__ == '__main__':
 
     """ declare myself """
 
     print("=============================================================")
-    print("|    Measure features - Simple tool for simple jobs v.1.0   |")
+    print("|    Measure features - Simple tool for simple jobs v.1.1   |")
     print("|                                                           |")
     print("|    by: Barbara Piskur and Janez Presern (c) 2016, 2017    |")
     print("|                                                           |")
@@ -550,13 +732,26 @@ if __name__ == '__main__':
         stay = True
         while stay:
             fig.canvas.manager.window.iconify()
-            ax2.set_title('Now what? Measure (A)rea, Measure (L)ength, (C)lose image')
-            now_what = input('Now what? Measure (A)rea, Measure (L)ength, (C)lose image\n')
+            ax2.set_title('Now what? Measure (A)rea, count (B)ees, cubital (I)ndex, Measure (L)ength, (C)lose image')
+            now_what = input('Now what? Measure (A)rea, count(B)ees, cubital (I)ndex, Measure (L)ength, (C)lose image\n')
             if now_what == 'a':
                 fig.canvas.manager.window.deiconify()
                 fig.canvas.manager.window.tkraise()
                 storage, storage_short, counter = select_area(title_fn, fig, ax2, storage, storage_short, im_index,
                                                               pix_size, counter)
+
+            elif now_what == 'b':
+                fig.canvas.manager.window.deiconify()
+                fig.canvas.manager.window.tkraise()
+                stretched = stretch_comb(filename, fig, ax2)
+                storage, storage_short, counter = select_area(title_fn, fig, ax2, storage, storage_short, im_index,
+                                                              pix_size, counter)
+            elif now_what == 'i':
+                fig.canvas.manager.window.deiconify()
+                fig.canvas.manager.window.tkraise()
+                storage, storage_short, counter = measure_ci(title_fn, fig, ax2, storage, storage_short, pix_size,
+                                                             counter)
+
             elif now_what == 'l':
                 fig.canvas.manager.window.deiconify()
                 fig.canvas.manager.window.tkraise()
